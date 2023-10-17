3ec8104f589450287e4842911b9bdc4a0fb80bc2
Jason Gatsby <3163941+DeFiTON@users.noreply.github.com>
2023-10-17 09:59:24 +0300

    Add files via upload

---
+++ WebViewSample/app/src/main/java/com/smoservice/medianews/MyFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.smoservice.medianews
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
